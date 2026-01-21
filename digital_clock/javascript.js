3cedd3307e9ceb003441e271fdfd0a6629f66aab
Kriti Gera <kritig0206@gmail.com>
2026-01-21 19:20:31 +0530

    Add BMI calculator, digital clock and guessing game projects

---
+++ digital_clock/javascript.js
@@ -0,0 +1,8 @@
+const clock=document.getElementById('clock')
+
+
+setInterval(function(){ 
+  let date=new Date()
+  clock.innerHTML=date.toLocaleTimeString();
+ // console.log(date.toLocaleTimeString)
+},1000)
